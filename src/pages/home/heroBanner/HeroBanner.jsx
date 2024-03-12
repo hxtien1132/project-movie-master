1a37d430c4d9a8328bd13881e98798d5b9598bdb
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2024-03-12 23:36:42 +0700

    update

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -11,95 +11,151 @@ import { fetchDataFromApi } from "../../../utils/api";
 import "swiper/swiper-bundle.css";
 
 const HeroBanner = () => {
+  const [videoId, setVideoId] = useState(null);
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
   const [endpoint, setEndpoint] = useState("movie");
   const [background, setBackground] = useState("");
   const [data, setData] = useState({});
   const { url } = useSelector((state) => state.home);
   useEffect(() => {
+    const video = (data) => {
+      fetchDataFromApi(`/movie/${data?.id}/videos`).then((res) => {
+        setVideoId(res.results?.[1]?.key || null);
+      });
+    };
     fetchDataFromApi(`/${endpoint}/upcoming`)
       .then((res) => {
         const obj = res?.results?.[Math.floor(Math.random() * 10)];
         setData(obj);
+        video(obj);
       })
       .catch((err) => console.log(err));
   }, []);
   console.log(data);
-  // const obj = data?.results?.slice(0,5);
-  // useEffect(() => {
-  //   const bg = url.backdrop + data?.backdrop_path;
-  //   setBackground(bg);
-  // }, [data, background]);
+  useEffect(() => {
+    const bg = url.backdrop + data?.backdrop_path;
+    setBackground(bg);
+  }, [data, background]);
 
-  // const searchQueryHandler = (event) => {
-  //   if (event.key === "Enter" && query.length > 0) {
-  //     navigate(`/search/${query}`);
-  //   }
-  // };
-
-  return <div className="heroBanner">{data && <Item data={data} />}</div>;
-};
-
-const Item = (props) => {
-  const data = props.data;
-  const [videoId, setVideoId] = useState(null);
-  const navigate = useNavigate();
-  const [show, setShow] = useState(false);
-  const { url } = useSelector((state) => state.home);
-  const bg = url.backdrop + data?.backdrop_path;
   const watchTrailer = () => {
     setShow(true);
   };
-  fetchDataFromApi(`/movie/${data?.id}/videos`).then((res) => {
-    setVideoId(res.results?.[1]?.key || null);
-  });
+
   return (
-    <>
-      <div className="backdrop-img">
-        <Img src={bg} className="img" />
-      </div>
-      <div className="opacity-layer"></div>
-      <ContentWrapper>
-        <div className="heroBannerContent">
-          <div className="posterBlock">
-            <span className="title">{data?.title}</span>
-            <div className="description">
-              <div className="infoItem">
-                <span className="text">
-                  {dayjs(data?.release_date).format("MMM D, YYYY")}
-                </span>
-              </div>
-              <div className="infoItem">
-                <span className="text bold">Votes: </span>
-                <span className="text">{data?.vote_average?.toFixed(1)}</span>
-              </div>
-              <div className="infoItem">
-                <span className="text bold">Views: </span>
-                <span className="text">{data?.popularity}</span>
-              </div>
-            </div>
-            <div className="buttons">
-              <button
-                className="btn btn-wn"
-                onClick={() => navigate(`/movie/${data?.id}`)}
-              >
-                Watch now
-              </button>
-              <button className="btn btn-wt" onClick={watchTrailer}>
-                Watch trailer
-              </button>
-            </div>
+    <div className="heroBanner">
+      {data && (
+        <>
+          <div className="backdrop-img">
+            <Img src={background} className="img" />
           </div>
-        </div>
-        <VideoPopup
-          show={show}
-          setShow={setShow}
-          videoId={videoId}
-          setVideoId={setVideoId}
-        />
-      </ContentWrapper>
-    </>
+          <div className="opacity-layer"></div>
+          <ContentWrapper>
+            <div className="heroBannerContent">
+              <div className="posterBlock">
+                <span className="title">{data?.title}</span>
+                <div className="description">
+                  <div className="infoItem">
+                    <span className="text">
+                      {dayjs(data?.release_date).format("MMM D, YYYY")}
+                    </span>
+                  </div>
+                  <div className="infoItem">
+                    <span className="text bold">Votes: </span>
+                    <span className="text">{data?.vote_average?.toFixed(1)}</span>
+                  </div>
+                  <div className="infoItem">
+                    <span className="text bold">Views: </span>
+                    <span className="text">{data?.popularity}</span>
+                  </div>
+                </div>
+                <div className="buttons">
+                  <button
+                    className="btn btn-wn"
+                    onClick={() => navigate(`/movie/${data?.id}`)}
+                  >
+                    Watch now
+                  </button>
+                  <button className="btn btn-wt" onClick={watchTrailer}>
+                    Watch trailer
+                  </button>
+                </div>
+              </div>
+            </div>
+            <VideoPopup
+              show={show}
+              setShow={setShow}
+              videoId={videoId}
+              setVideoId={setVideoId}
+            />
+          </ContentWrapper>
+        </>
+      )}
+    </div>
   );
 };
+
+// const Item = (props) => {
+//   const data = props.data;
+//   const [videoId, setVideoId] = useState(null);
+//   const navigate = useNavigate();
+//   const [show, setShow] = useState(false);
+//   const { url } = useSelector((state) => state.home);
+//   const bg = url.backdrop + data?.backdrop_path;
+//   const watchTrailer = () => {
+//     setShow(true);
+//   };
+//   fetchDataFromApi(`/movie/${data?.id}/videos`).then((res) => {
+//     setVideoId(res.results?.[1]?.key || null);
+//   });
+//   return (
+//     <>
+//       <div className="backdrop-img">
+//         <Img src={bg} className="img" />
+//       </div>
+//       <div className="opacity-layer"></div>
+//       <ContentWrapper>
+//         <div className="heroBannerContent">
+//           <div className="posterBlock">
+//             <span className="title">{data?.title}</span>
+//             <div className="description">
+//               <div className="infoItem">
+//                 <span className="text">
+//                   {dayjs(data?.release_date).format("MMM D, YYYY")}
+//                 </span>
+//               </div>
+//               <div className="infoItem">
+//                 <span className="text bold">Votes: </span>
+//                 <span className="text">{data?.vote_average?.toFixed(1)}</span>
+//               </div>
+//               <div className="infoItem">
+//                 <span className="text bold">Views: </span>
+//                 <span className="text">{data?.popularity}</span>
+//               </div>
+//             </div>
+//             <div className="buttons">
+//               <button
+//                 className="btn btn-wn"
+//                 onClick={() => navigate(`/movie/${data?.id}`)}
+//               >
+//                 Watch now
+//               </button>
+//               <button className="btn btn-wt" onClick={watchTrailer}>
+//                 Watch trailer
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//         <VideoPopup
+//           show={show}
+//           setShow={setShow}
+//           videoId={videoId}
+//           setVideoId={setVideoId}
+//         />
+//       </ContentWrapper>
+//     </>
+//   );
+// };
 
 // const HeroBanner = () => {
 //   SwiperCore.use([Autoplay]);
