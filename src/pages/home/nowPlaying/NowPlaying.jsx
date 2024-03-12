6583c99487ab80be39a9d69b96f9bb1539cc81af
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2024-03-12 21:42:42 +0700

    update

--- src/pages/home/nowPlaying/NowPlaying.jsx
+++ src/pages/home/nowPlaying/NowPlaying.jsx
@@ -8,17 +8,17 @@ import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
 
 const NowPlaying = () => {
-  const [endpoint, setEndpoint] = useState("day");
-  const { data, loading } = useFetch(`/tv/airing_today`);
+  const [endpoint, setEndpoint] = useState("airing_today");
+  const { data, loading } = useFetch(`/tv/${endpoint}`);
   const onTabChange = (tab) => {
-    setEndpoint(tab === "Day" ? "day" : "week");
+    setEndpoint(tab === "Tv channel" ? "airing_today" : "on_the_air");
   };
 
   return (
     <div className="carouselSection">
       <ContentWrapper>
         <span className="carouselTitle">TV shows </span>
-        {/* <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} /> */}
+        <SwitchTabs data={["Tv channel", "Talk show"]} onTabChange={onTabChange} />
       </ContentWrapper>
       <Carousel data={data?.results} loading={loading} />
     </div>
