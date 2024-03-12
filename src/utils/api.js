860d0f6a06b3ecd54795c73e738a08a7ed1f9845
hxtien1132 <78005808+hxtien1132@users.noreply.github.com>
2024-03-13 02:08:54 +0700

    ypdate

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const VITE_APP_TMDB_TOKEN =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTE1NDllY2E5ODQyNWQ4MmQzZDRiZWRiNzg5ZTAyYiIsInN1YiI6IjYzNTNhNTVjODgwYzkyMDA3OTZkNWRkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AGW7omIPI0kSJXM0xvSxuMdQNLqNSMfUlN2aW5lWJ - 8";
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = VITE_APP_TMDB_TOKEN;
+const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
   Authorization: "bearer " + TMDB_TOKEN,
